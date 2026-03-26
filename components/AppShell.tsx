46cce025e3244162166dbb8890cd583a7488a408
Jonah Manning <jonah@wellsonyx.com>
2026-03-26 16:31:20 -0400

    Revert "Add Onyx Semiconductor page at /fab"
    This reverts commit 92608f2e1f18ceebcff253123aca3eec3a809b51.

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -19,7 +19,7 @@ export function AppShell({
   children: React.ReactNode;
 }) {
   const pathname = usePathname();
-  const isChromeless = pathname === '/' || pathname === '/demo' || pathname === '/site' || pathname === '/fab';
+  const isChromeless = pathname === '/' || pathname === '/demo' || pathname === '/site';
 
   if (isChromeless) {
     return <>{children}</>;
